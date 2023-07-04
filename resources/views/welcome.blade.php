9e16b4c90e1630b2f00cb7f37489a40756a6c7fa
yukiya4242 <126574597+yukiya4242@users.noreply.github.com>
2023-07-04 01:24:17 +0000

    none

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,10 +29,11 @@
        <header class="text-gray-600 body-font">
         <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
             <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+                <img src="{{ asset('img/clowdshare.png') }}" class="w-10 h-10 text-white p-2 bg-indigo-50 rounded-full">
+
                     <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
                 </svg>
-                <span class="ml-3 text-xl">Tailblocks</span>
+                <span class="ml-3 text-xl">CLOUD SHARE</span>
             </a>
             <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
                 @if (Auth::check())
@@ -51,8 +52,8 @@
 <section class="text-gray-600 body-font">
   <div class="container px-5 py-24 mx-auto flex flex-wrap">
     <div class="flex w-full mb-20 flex-wrap">
-      <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 lg:w-1/3 lg:mb-0 mb-4">Master Cleanse Reliac Heirloom</h1>
-      <p class="lg:pl-6 lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom.</p>
+      <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 lg:w-1/3 lg:mb-0 mb-4">あなたの撮った上空の写真をみんなにシェアしよう</h1>
+      <p class="lg:pl-6 lg:w-2/3 mx-auto leading-relaxed text-base">空からの視点は、私たちが普段見慣れた風景を全く新しい角度から見る機会を提供します。あなたの撮った上空の写真は、ただの風景ではなく、物語、感情、そして新たな視点を共有する手段です。私たちのアプリを通じて、その一瞬一瞬の感動を他の人々と共有し、同じ空の下で結ばれたコミュニティを形成しましょう。あなたの視点が、誰かの新たな発見やインスピレーションの源になるかもしれません。</p>
     </div>
 
     <div class="flex flex-wrap md:-m-2 -m-1">
@@ -88,10 +89,10 @@
       <footer class="text-gray-600 body-font">
           <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
             <a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+              <img src="{{ asset('img/clowdshare.png') }}" class="w-10 h-10 text-white p-2 bg-indigo-50 rounded-full">
                 <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
               </svg>
-              <span class="ml-3 text-xl">Tailblocks</span>
+              <span class="ml-3 text-xl">CLOUD SHARE</span>
             </a>
             <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2020 Tailblocks —
               <a href="https://twitter.com/knyttneve" class="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@knyttneve</a>
