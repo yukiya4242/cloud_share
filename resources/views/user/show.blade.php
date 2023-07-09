2edf7c7883b4aee98e5a4dea7fb71fa049851d3a
yukiya4242 <126574597+yukiya4242@users.noreply.github.com>
2023-07-09 09:30:23 +0000

    Google map API

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -9,29 +9,17 @@
       <!--<p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them.</p>-->
     </div>
     <div class="flex flex-wrap -m-4">
-        @foreach($photos as $photo)
-      <div class="p-4 lg:w-1/4 md:w-1/2">
-        <div class="h-full flex flex-col items-center text-center">
-          <img alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="{{ Storage::url($photo->filename )}}" style="object-fit: cover; width: 200px; height:200px;">
-          <div id="map_{{ $photo->id }}" style="height: 200px; width: 100%;"></div>
+       @foreach($photos as $photo)
+         <div class="p-4 lg:w-1/4 md:w-1/2">
+           <div class="h-full flex flex-col items-center text-center">
+             <img alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="{{ Storage::url($photo->filename )}}" style="object-fit: cover; width: 200px; height:200px;">
 
-          <script>
-              function initMap{{ $photo->id }}() {
-                  var myLatLng = {lat: -25.363, lng: 131.044};
 
-                  var map = new google.maps.Map(document.getElementById('map_{{ $photo->id }}'), {
-                    zoom: 4,
-                    center: myLatLng
-                  });
+            <div id="map-{{ $photo->id }}" style="height: 200px; width: 100%;"></div>
 
-                  var marker = new google.maps.Marker({
-                    position: myLatLng,
-                    map: map,
-                    title: 'Hello World!'
-                  });
-              }
-              initMap{{ $photo->id }}();
-          </script>
+          <script src="{{ asset('/js/result.js') }}"></script>
+          <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAPS_API_KEY') }}&callback=initAllMaps"></script>
+
 
           <div class="w-full">
             <h2 class="title-font font-medium text-lg text-gray-900">タイトル: {{ $photo->title }}</h2>
@@ -65,20 +53,5 @@
      @endforeach
   </div>
 </section>
-
-@php
-  $initMapFunctions = array_map(function($photo) {
-    return 'initMap' . $photo->id . '()';
-  }, $photos->all());
-  $initAllMapsFunctionBody = implode(';', $initMapFunctions);
-@endphp
-
-<script>
-  function initAllMaps() {
-    {!! $initAllMapsFunctionBody !!}
-  }
-</script>
-
-<script src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAPS_API_KEY') }}&callback=initAllMaps" async defer></script>
 
 @endsection
