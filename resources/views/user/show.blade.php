59332d5bf16d5523c8ebe8bcbf5f5ebcea8f9780
yukiya4242 <126574597+yukiya4242@users.noreply.github.com>
2023-07-17 02:47:46 +0000

    Edit users/show

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -13,32 +13,108 @@ let locations = [
 </script>
  <!--<script src="{{ asset('/js/result.js') }}"></script>-->
 
-<section class="text-gray-600 body-font">
-  <div class="container px-5 py-24 mx-auto">
-    <div class="flex flex-col text-center mb-20">
-      <h1 class="text-2xl font-medium title-font mb-4 text-gray-900">{{ $user->name }}さんの投稿</h1>
-      <!--<p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them.</p>-->
-    </div>
-    <div class="flex flex-wrap -m-4">
-       @foreach($photos as $photo)
-         <div class="p-4 lg:w-1/4 md:w-1/2">
-           <div class="h-full flex flex-col items-center text-center">
-             <img alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="{{ Storage::url($photo->filename )}}" style="object-fit: cover; width: 200px; height:200px;">
+<section class="flex flex-row flex-wrap mx-auto">
+<!-- Card Component -->
+ @foreach($photos as $photo)
+  <div
+    class="transition-all duration-150 flex w-full px-4 py-6 md:w-1/4 lg:w-1/4"
+  >
+    <div
+      class="flex flex-col items-stretch min-h-full pb-4 mb-6 transition-all duration-150 bg-white rounded-lg shadow-lg hover:shadow-2xl"
+    >
+      <div class="md:flex-shrink-0">
+        <img
+          src="{{ Storage::url($photo->filename )}}"
+          alt="Blog Cover"
+          class="object-cover object-center w-full h-full block rounded-lg"
+          style="height: 200px; width: 300px;"
+        />
+      </div>
+      <div class="flex items-center justify-between px-4 py-2 overflow-hidden">
+        <span class="text-xs font-medium text-blue-900 uppercase">
+          <a href="#" class="show-map" data-lat="{{ $photo->latitude }}" data-lng="{{ $photo->longitude }}" data-photo-id="{{ $photo->id }}">Mapを表示</a>
+        </span>
+        <div class="flex flex-row items-center">
+          <div
+            class="text-xs font-medium text-gray-500 flex flex-row items-center mr-2"
+          >
+            <svg
+              class="w-4 h-4 mr-1"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+              ></path>
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+              ></path>
+            </svg>
+          </div>
 
-             <!-- Map link -->
-             <a href="#" class="show-map" data-lat="{{ $photo->latitude }}" data-lng="{{ $photo->longitude }}" data-photo-id="{{ $photo->id }}">Mapを表示</a>
+          <div
+            class="text-xs font-medium text-gray-500 flex flex-row items-center mr-2"
+          >
+            <svg
+              class="w-4 h-4 mr-1"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
+              ></path>
+            </svg>
+          </div>
 
-             <div class="w-full">
-               <h2 class="title-font font-medium text-lg text-gray-900">タイトル: {{ $photo->title }}</h2>
-               <h3 class="text-gray-500 mb-3">名前: {{ $user->name }}</h3>
-               <p class="mb-4">{{ $photo->caption }}</p>
-
-               <form method="POST" action="{{ route('photo.destroy', ['id' => $photo->id]) }}">
+          <div
+            class="text-xs font-medium text-gray-500 flex flex-row items-center"
+          >
+            <svg
+              class="w-4 h-4 mr-1"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5"
+              ></path>
+            </svg>
+          </div>
+        </div>
+      </div>
+      <hr class="border-gray-300" />
+      <div class="flex flex-wrap items-center flex-1 px-4 py-1 text-center mx-auto">
+        <h2 class="text-2xl font-bold tracking-normal text-gray-800">Title: {{ $photo->title }}</h2>
+      </div>
+      <hr class="border-gray-300" />
+      <p class="flex flex-row flex-wrap w-full px-4 py-2 overflow-hidden text-sm text-justify text-gray-700">Capation: {{ $photo->caption }}</p>
+      <hr class="border-gray-300" />
+      <section class="px-2 py-1 pt-2">
+        <div class="flex items-center justify-between">
+          <div class="flex items-center flex-1">
+　　　　　　　<form method="POST" action="{{ route('photo.destroy', ['id' => $photo->id]) }}">
                    @csrf
                    @method('DELETE')
 
                @if(Auth::user()->id == $photo->user_id)
-                   <button type="submit" class="btn btn-danger bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onclick="return confirm('本当に削除しますか')">
+                   <button type="submit" class="btn btn-danger bg-red-500 hover:bg-red-700 text-white mr-4 font-bold py-1 px-2 rounded text-sm" onclick="return confirm('本当に削除しますか')">
                        削除
                    </button>
                @endif
@@ -49,17 +125,19 @@ let locations = [
                    @method('GET')
 
                @if(Auth::user()->id == $photo->user_id)
-                   <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+                   <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-blue-700 rounded text-sm">
                        編集
                    </button>
                @endif
                </form>
-             </div>
-           </div>
-         </div>
-       @endforeach
+
+          </div>
+        </div>
+      </section>
     </div>
   </div>
+  @endforeach
+ </div>
 </section>
 
 <!-- モーダルの基本的なHTML -->
@@ -109,3 +187,5 @@ $(document).ready(function() {
 </script>
 
 @endsection
+
+
